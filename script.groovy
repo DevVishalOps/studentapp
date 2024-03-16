0f56fd24e2608cdea2f5d03e3bbf9bb06b6cb69d
vishal <rajput.vishal.rajesh@gmail.com>
2024-03-16 11:29:33 +0530

    first commit

---
+++ script.groovy
@@ -0,0 +1,21 @@
+pipeline {
+    agent any
+
+    stages {
+
+        stage('Pull') {
+            steps {
+                git 'https://github.com/chetansomkuwar254/studentapp.ui.git'
+                echo 'Hello World'
+            }
+        }
+
+        stage('Build') {
+            steps {
+                sh 'mvn clean test'
+                echo 'Hello World'
+            }
+        }
+
+    }
+}
